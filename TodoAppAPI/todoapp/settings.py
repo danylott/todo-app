12a13e18201c15b2f22a396c58005513bc568523
Daniel <d.tuytuyshkin@gmail.com>
2020-08-04 13:34:21 +0200

    todo app vue-django ready

--- TodoAppAPI/todoapp/settings.py
+++ TodoAppAPI/todoapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api',
 ]
 
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'todoapp.urls'
@@ -70,6 +73,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'todoapp.wsgi.application'
+
+
+CORS_ORIGIN_WHITELIST = [
+    "http://127.0.0.1:8081",
+    "http://localhost:8081",
+]
 
 
 # Database
